f966e5d311df1803fa036670c35b6232288bc1c7
Vakor <myksv200@gmail.com>
2021-11-02 10:18:53 +0300

    02.11 Configuration crt

---
+++ Vakor.GeneticAlgorithm/Vakor.GeneticAlgorithm.Lib/Configurations/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace Vakor.GeneticAlgorithm.Lib.Configurations
+{
+    public class Configuration
+    {
+        public int AlgorithmIterationCount { get; set; } = 1000;
+        public double MutationsPossibility { get; set; } = .05;
+        public double CrossoverPoint { get; set; } = .3;
+        public int ItemsCount { get; set; }
+    }
+}
