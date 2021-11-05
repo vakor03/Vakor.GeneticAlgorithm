3a85bea54ea34be0ef5d651547dc4fba1c3e2d6a
Vakor <myksv200@gmail.com>
2021-11-05 22:33:30 +0300

    05.11 Program

--- Vakor.GeneticAlgorithm/Vakor.GeneticAlgorithm.ConsoleApp/Program.cs
+++ Vakor.GeneticAlgorithm/Vakor.GeneticAlgorithm.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Vakor.GeneticAlgorithm.Lib.EvolutionAlgorithms;
+using Vakor.GeneticAlgorithm.Lib.Items;
 
 namespace Vakor.GeneticAlgorithm.ConsoleApp
 {
@@ -6,7 +9,33 @@ namespace Vakor.GeneticAlgorithm.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IEvolutionAlgorithm evolutionAlgorithm = new EvolutionAlgorithm();
+            List<IItem> items = FormInitialItems(100);
+
+            for (int i = 0; i < 100; i++)
+            {
+                int generation = i * 5;
+                var backpack = evolutionAlgorithm.SolveBackpackTask(250, items, generation);
+                Console.WriteLine(
+                    $"Generation {generation}; Used capacity:{backpack.UsedCapacity}; Value:{backpack.ItemsValue}");
+            }
+        }
+
+        private static List<IItem> FormInitialItems(int itemsCount)
+        {
+            Random random = new Random();
+            List<IItem> items = new List<IItem>();
+            for (int i = 0; i < itemsCount; i++)
+            {
+                IItem item = new Item
+                {
+                    Capacity = random.Next(1, 25),
+                    Value = random.Next(2, 30)
+                };
+                items.Add(item);
+            }
+
+            return items;
         }
     }
 }
