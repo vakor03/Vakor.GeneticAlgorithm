ebd89fdf9cb11e5110a5de6587249254b012c37a
Vakor <myksv200@gmail.com>
2021-11-05 22:32:42 +0300

    05.11 Tests

---
+++ Vakor.GeneticAlgorithm/Vakor.GeneticAlgorithm.Lib/Items/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace Vakor.GeneticAlgorithm.Lib.Items
+{
+    public class Item:IItem
+    {
+        public string Name { get; set; }
+        public double Capacity { get; set; }
+        public double Value { get; set; }
+    }
+}
