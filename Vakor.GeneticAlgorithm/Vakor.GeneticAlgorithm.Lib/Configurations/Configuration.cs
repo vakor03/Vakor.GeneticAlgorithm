b99c9876b2d996220d2d899fd0fcebe9f35cd519
Vakor <myksv200@gmail.com>
2021-11-05 22:33:00 +0300

    05.11 Configuration

--- Vakor.GeneticAlgorithm/Vakor.GeneticAlgorithm.Lib/Configurations/Configuration.cs
+++ Vakor.GeneticAlgorithm/Vakor.GeneticAlgorithm.Lib/Configurations/Configuration.cs
@@ -2,9 +2,8 @@
 {
     public class Configuration
     {
-        public int AlgorithmIterationCount { get; set; } = 1000;
+        public int GenerationCount { get; set; } = 1000;
         public double MutationsPossibility { get; set; } = .05;
         public double CrossoverPoint { get; set; } = .3;
-        public int ItemsCount { get; set; }
     }
 }
